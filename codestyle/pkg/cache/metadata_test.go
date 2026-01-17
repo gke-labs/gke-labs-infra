8d80202b76fdcb7c57b7b184383a30b6de9d287e
codebot-robot <codebot-robot@google.com>
2026-01-17 14:49:05 +0000

    fixup! Fix build on macOS by replacing syscall.Stat_t.Mtim with os.FileInfo.ModTime

--- codestyle/pkg/cache/metadata_test.go
+++ codestyle/pkg/cache/metadata_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cache
 
 import (
@@ -31,7 +45,7 @@ func TestGetMetadata(t *testing.T) {
 	}
 
 	// Allow for small differences if any, but they should be exact for UnixNano usually
-    // However, if the FS implementation doesn't support nanoseconds, both might be truncated, which is fine as long as they match.
+	// However, if the FS implementation doesn't support nanoseconds, both might be truncated, which is fine as long as they match.
 	if meta.Mtime != fi.ModTime().UnixNano() {
 		t.Errorf("expected mtime %d, got %d", fi.ModTime().UnixNano(), meta.Mtime)
 	}
